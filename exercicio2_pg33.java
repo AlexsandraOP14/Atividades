e8d9ec34bf4aa0202e234df85984f17ddd2e4025
Alexsandra Oliveira Pacheco <107128820+AlexsandraOP14@users.noreply.github.com>
2022-09-10 21:26:50 -0300

    Add files via upload

---
+++ exercicio2_pg33.java
@@ -0,0 +1,11 @@
+//Alexsandra Oliveira e Isadora Oliveira 3.54
+//Exercício 2 página 33.
+
+public class exercicio2 { 
+    public static void main(String[] args) {    
+        for (int num = 2; num <= 123; num++) 
+            if ((num % 2) == 0){
+                System.out.println(num + " é um número primo.");
+        }  
+    }
+}
